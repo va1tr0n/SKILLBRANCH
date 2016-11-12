0b6a455aab6b649c928772b0d35e7fc6e372f5bd
valentina3 <valentina.workmail@gmail.com>
2016-11-12 17:22:14 +0700

    task 2b

--- index.js
+++ index.js
@@ -8,9 +8,38 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+// Задание 2А
 app.get('/t', (req, res) => {
   const sum = (+req.query.a || 0) + (+req.query.b || 0);
   res.send(sum.toString());
+});
+
+// Задание 2В
+app.get('/f', (req, res) => {
+  var fullname = req.query.fullname.trim().replace(/\s+/g," ");
+  if(fullname.length > 0){
+    fullname = fullname.split(/\s/);
+    switch (fullname.length) {
+      case 0:{
+        res.send('Invalid fullname');
+      }break;
+      case 1:{
+        res.send(fullname[0]);
+      }break;
+      case 2:{
+        res.send(fullname[1] +" "+ fullname[0].substr(0,1) + ".");
+      }break;
+      case 3:{
+        res.send(fullname[2] +" "+ fullname[0].substr(0,1) + ". " + fullname[1].substr(0,1)+".");
+      }
+        break;
+      default:{
+        res.send('Invalid fullname');
+      }
+    }
+  }else{
+    res.send('Invalid fullname');
+  }
 });
 
 app.listen(3000, () => {
