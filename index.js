5089048ca913f26292ad44a4931694977afe017c
valentina3 <valentina.workmail@gmail.com>
2016-11-12 17:42:22 +0700

    task 2C

--- index.js
+++ index.js
@@ -42,6 +42,18 @@ app.get('/f', (req, res) => {
   }
 });
 
+// Задание 2С
+app.get('/c', (req, res) => {
+    let url = req.query.username || '';
+    let username = url.match(/\b\/@?([\w\.]+)|^@?([\w\.]+)$/);
+
+    if (username === null) {
+        return res.send('Invalid username');
+    }
+
+    res.send('@' + (username[1] || username[2]));
+});
+
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
